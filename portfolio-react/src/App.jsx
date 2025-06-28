80bcb54541d4402683f391036ef0be7e4b1888ab
karriv18 <krlclarence@gmail.com>
2025-06-28 20:33:33 +0800

    Portfolio initial commit

---
+++ portfolio-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import Hero from './Components/Hero/Hero';
+
+import './index.css';
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+        </div>
+    )
+}
+
+export default App
---
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Hero.css';
+import profile_img from '../../assets/profile_img.svg';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
+
+const Hero = () => {
+    const [text] = useTypewriter({
+        words: ['Karl Clarence Rivera'],
+        typeSpeed: 100,
+        deleteSpeed: 0,
+        delaySpeed: 10000
+    });
+
+    return (
+        <div className='hero'>
+            {/* <img src={profile_img} alt="" /> */}
+            {/* Pending animation */}
+            <h1>I'm <span> {text} </span><span><Cursor /></span></h1>
+            <p>A dedicated <span>Software Developer</span> with a strong passion for turning complex ideas into efficient, scalable, and user-focused web solutions.</p>
+            <div className='hero-action'>
+                <div className='hero-connect'>
+                    Connect with Me
+                </div>
+                <div className='hero-resume'>
+                    My Resume
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
